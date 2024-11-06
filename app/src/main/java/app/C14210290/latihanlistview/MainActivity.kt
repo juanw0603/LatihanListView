3f45450a8c006e26b29231d161487e7b0a263cfa
juanWijaya0603 <juanw0603@gmail.com>
2024-11-06 19:10:17 +0700

    update 1

--- app/src/main/java/app/C14210290/latihanlistview/MainActivity.kt
+++ app/src/main/java/app/C14210290/latihanlistview/MainActivity.kt
@@ -1,6 +1,10 @@
 package app.C14210290.latihanlistview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,40 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        var data = mutableListOf<String>()
+
+        data.addAll(listOf("1", "2", "3", "4", "5"))
+
+
+        val lvAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, data
+        )
+
+        val _lv1 = findViewById<ListView>(R.id.lv1)
+
+        _lv1.adapter = lvAdapter
+
+        val _btnTambah = findViewById<Button>(R.id.button_nambahData)
+
+        _btnTambah.setOnClickListener {
+            val dtAkhir = Integer.parseInt(data.get(data.size - 1)) + 1
+            data.add(dtAkhir.toString())
+
+            lvAdapter.notifyDataSetChanged()
+        }
+
+        _lv1.setOnItemClickListener() { parent, view, position, id ->
+            Toast.makeText(
+                this, "data ke ${data[position]}", Toast.LENGTH_SHORT).show()
+        }
+        val _btnHapus = findViewById<Button>(R.id.button_hapusData)
+        _btnHapus.setOnClickListener {
+            data.removeFirst()
+            lvAdapter.notifyDataSetChanged()
         }
     }
 }
